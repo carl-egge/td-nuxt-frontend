8a862a0f89f03f50f1cbe2fd261bb83b537ecbd9
Carl Egge <carl.egge@web.de>
2023-08-22 13:36:23 +0200

    add content to vermietung + schule

--- pages/vermietung.vue
+++ pages/vermietung.vue
@@ -1,6 +1,51 @@
 <template>
     <div>
         <PageHero title="Vermietung" />
+        <v-container>
+            <v-row>
+                <v-col>
+                    <h4>Für alle die Raum brauchen || Regelmäßig oder einmalig</h4>
+                    <h4>HOCHZEITEN | GEBURTSTAGE | PROBEN</h4>
+
+                    <ul>
+                        <li>Theaterproben</li>
+                        <li>Navigationstraining</li>
+                        <li>Vorsprechvorbereitung</li>
+                        <li>Monologarbeit</li>
+                        <li>Camera Acting</li>
+                        <li>Anlegemanöver</li>
+                        <li>Sprechübungen</li>
+                        <li>Bühnenpatent</li>
+                    </ul>
+
+                    <h4>Deck I (großer Saal | 100m²)*</h4>
+                    <p>montags bis donnerstags</p>
+                    <p>10 bis 14 Uhr || inkl. Bühne (mind. 4m mal 4m) | kleine mobile Tonanlage | Arbeitslicht</p>
+
+                    <h4>Deck II (Studiobühne | 40m²)*</h4>
+                    <p>montags bis donnerstags</p>
+                    <p>10 bis 14 Uhr || inkl. kleine mobile Tonanlage | Arbeitslicht</p>
+
+                    <p>Bitte melden Sie sich über unser Kontaktformular an oder schreiben uns direkt an:
+                        <a href="mailto:info@theaterdeck.de">info@theaterdeck.de</a>
+                    </p>
+
+                    <h4>Was noch möglich ist:</h4>
+                    <ul>
+                        <li>Hochzeiten</li>
+                        <li>Geburtstage</li>
+                        <li>Konfirmationen</li>
+                        <li>Trauungen</li>
+                        <li>und und und</li>
+                    </ul>
+
+                    <p>sämtliche Möglichkeiten und Konditionen unter: <a href="https://www.luettliv.de">www.luettliv.de</a>
+                    </p>
+
+                    <p>*Es gelten die Allgemeinen Geschäftsbedingungen</p>
+                </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
 
