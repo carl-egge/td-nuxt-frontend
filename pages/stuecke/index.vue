fbb2b878d26d08a68fae7220bdbca888fc0c16d5
Carl Egge <carl.egge@web.de>
2023-08-22 09:43:26 +0200

    catch event fetch error

--- pages/stuecke/index.vue
+++ pages/stuecke/index.vue
@@ -4,9 +4,11 @@
         <div v-if="eventsStore.countEvents > 0" v-for="event in eventsStore.getAll">
             <EventListItem :event="event" />
         </div>
-        <div v-else>
-            <p>Keine Events gefunden</p>
-            <v-btn to="/stuecke" nuxt class="my-4"> Erneut Versuchen </v-btn>
+        <div v-else class="text-center">
+            <p class="my-6">Keine Events gefunden</p>
+            <v-btn class="my-4">
+                <a href="/stuecke"> Erneut Versuchen </a>
+            </v-btn>
         </div>
     </div>
 </template>
@@ -21,9 +23,6 @@
 
 import { useEventsStore } from '~/store/events';
 const eventsStore = useEventsStore();
-if (eventsStore.events.length === 0) {
-    throw createError({ statusCode: 404, statusMessage: 'No Events found' })
-}
 </script>
 
 <style lang="scss" scoped></style>
